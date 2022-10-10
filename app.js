787818c5bec0a76defb42fc9191c9af7b18da915
N000055\Yvonne Kuo <yvonn-kuo@dactp.com.tw>
2022-10-11 01:13:34 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const mongoose = require('mongoose')
-
+const Todo = require('./models/todo')
 const exphbs = require('express-handlebars')
 const app = express()
 
@@ -22,7 +22,10 @@ app.set('view engine', 'hbs')
 
 // 設定首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 // 設定 port 3000

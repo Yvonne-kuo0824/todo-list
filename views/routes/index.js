0eb1928f81e47cb54cce1abb3a5e0fa795e30393
N000055\Yvonne Kuo <yvonn-kuo@dactp.com.tw>
2022-10-12 21:48:01 +0800

    refactor: modify express routes

---
+++ views/routes/index.js
@@ -0,0 +1,11 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+
+const home = require('./modules/home')
+const todos = require('./modules/todos')
+
+router.use('/', home)
+router.use('/todos', todos)
+
+module.exports = router

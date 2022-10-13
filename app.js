d334afd22726bff89c73b6fe6338dc7e77d4213d
N000055\Yvonne Kuo <yvonn-kuo@dactp.com.tw>
2022-10-13 17:56:36 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-const Todo = require('./models/todo')
 
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
-
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -32,5 +20,5 @@ app.use(routes)
 
 // 設定 port 3000
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+  console.log('App is running on http://localhost:3000.')
 })
